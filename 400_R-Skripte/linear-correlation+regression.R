d7e56535a92e00b881af38fa1bb49fbd38f62e25
irina-grabhermeier <igr7222@students.fhv.at>
2019-12-07 17:55:03 +0100

    Update linear-correlation+regression.R
    Add linear-correlation+regression

--- 400_R-Skripte/linear-correlation+regression.R
+++ 400_R-Skripte/linear-correlation+regression.R
@@ -1,4 +1,5 @@
 
+#Zusammenhang Verständlichkeit und Geschwindigkeit der Vorlesung
 #Lineare Regressionsgerade bestimmen und 
 
 vorlesung_verstaendlich <- data[,"Vorlesung.verstaendlich"]
@@ -22,7 +23,8 @@ cor.test(vorlesung_verstaendlich, vorlesung_geschwindigkeit, method="pearson")
 # r = 0.8119...
 
 
-#lineare Regressionsgerade
+#Zusammenhang Umfang der Beispiele und Schwierigkeit
+#lineare Regressionsgerade 
 umfang_beispiele <- data[,"Uebung.Beispiele"]
 schwierigkeit_beispiele <- data[,"Uebung.schwierig"]
 
@@ -41,3 +43,91 @@ shapiro.test(schwierigkeit_beispiele)
 cor.test(umfang_beispiele, schwierigkeit_beispiele, method="spearman")
 
 # r = rho = 0.6139...    --Warnung kann ignoriert werden
+
+
+
+#Zusammenhang Unterricht abwechlungsreich und motivierend
+abwechlungsreich <- data[,"Unterricht.abwechslungsr"]
+motivierend <- data[, "Unterricht.motivierend"]
+
+plot(abwechlungsreich, motivierend)
+cor(abwechlungsreich,motivierend)
+
+lm(motivierend~abwechlungsreich) #zeigt y=kx+d, Rheienfolge Ausgabe - Werte: d dann k
+abline(lm(motivierend~abwechlungsreich)) #zeichnet lineare Regressionsgerade
+
+#Berechnung des Korrelationsfaktors r
+
+#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
+shapiro.test(abwechlungsreich)
+shapiro.test(motivierend)
+
+cor.test(abwechlungsreich, motivierend, method="pearson")
+
+# r = 0.43488...
+
+
+#Zusammenhang genügend Platz für Fragen und motivierend
+
+motivierend <- data[,"Unterricht.motivierend"]
+fragen <- data[,"Unterricht.Fragen"]
+
+plot(motivierend, fragen)
+cor(motivierend,fragen)
+
+lm(fragen~motivierend)
+abline(lm(fragen~motivierend)) 
+
+
+#Berechnung des Korrelationsfaktors r
+
+#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
+shapiro.test(motivierend)
+shapiro.test(fragen)
+
+cor.test(motivierend, fragen, method="pearson")
+
+# r = 0.517...
+
+#Zusammenhang abwechslungsreich und strukturiert
+
+abwechlungsreich <- data[,"Unterricht.abwechslungsr"]
+strukturiert <- data[, "Unterricht.strukturiert"]
+
+plot(abwechlungsreich, strukturiert)
+cor(abwechlungsreich,strukturiert)
+
+lm(strukturiert~abwechlungsreich) 
+abline(lm(strukturiert~abwechlungsreich)) 
+
+#Berechnung des Korrelationsfaktors r
+
+#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
+shapiro.test(abwechlungsreich)
+shapiro.test(strukturiert)
+
+cor.test(abwechlungsreich, strukturiert, method="pearson")
+
+# r = 0.3625...
+
+#Zusammenhang Termin & Zeit und Geschwindigkeit im Unterricht
+
+termin <- data[,"Termin"]
+geschwindigkeit <- data[, "Vorlesung.Geschwindigkeit"]
+
+plot(termin, geschwindigkeit)
+cor(termin,geschwindigkeit)
+
+lm(geschwindigkeit~termin) 
+abline(lm(geschwindigkeit~termin)) 
+
+#Berechnung des Korrelationsfaktors r
+
+#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
+shapiro.test(termin)
+shapiro.test(geschwindigkeit)
+
+cor.test(abwechlungsreich, strukturiert, method="spearman")
+
+# r = rho = 0.3631...
+

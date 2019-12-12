225d947f6c0d741f4e9dc3a27c45088834697a75
Dominic Luidold <dominic.luidold@students.fhv.at>
2019-12-12 13:00:09 +0100

    Add boxplot analysis of all data columns

---
+++ 400_R-Skripte/boxplotAnalysis.R
@@ -0,0 +1,35 @@
+#Boxplot - Gesamteindruck
+boxplot(data[,"Gesamteindruck"], col="#94d639", main="Boxplot: Gesamteindruck", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Raum
+boxplot(data[,"Raum"], col="#94d639", main="Boxplot: Raum", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Termin
+boxplot(data[,"Termin"], col="#94d639", main="Boxplot: Termin", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Unterricht.abwechslungsr
+boxplot(data[,"Unterricht.abwechslungsr"], col="#94d639", main="Boxplot: Unterricht.abwechslungsr", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Unterricht.motivierend
+boxplot(data[,"Unterricht.motivierend"], col="#94d639", main="Boxplot: Unterricht.motivierend", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Unterricht.Fragen
+boxplot(data[,"Unterricht.Fragen"], col="#94d639", main="Boxplot: Unterricht.Fragen", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Unterricht.strukturiert
+boxplot(data[,"Unterricht.strukturiert"], col="#94d639", main="Boxplot: Unterricht.strukturiert", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Vorlesung.verstaendlich
+boxplot(data[,"Vorlesung.verstaendlich"], col="#94d639", main="Boxplot: Vorlesung.verstaendlich", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Vorlesung.Geschwindigkeit
+boxplot(data[,"Vorlesung.Geschwindigkeit"], col="#94d639", main="Boxplot: Vorlesung.Geschwindigkeit", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Vorlesung.Praxis
+boxplot(data[,"Vorlesung.Praxis"], col="#94d639", main="Boxplot: Vorlesung.Praxis", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Uebung.Beispiele
+boxplot(data[,"Uebung.Beispiele"], col="#94d639", main="Boxplot: Uebung.Beispiele", ylab="Bewertung [Schulnote]")
+
+#Boxplot - Uebung.schwierig
+boxplot(data[,"Uebung.schwierig"], col="#94d639", main="Boxplot: Uebung.schwierig", ylab="Bewertung [Schulnote]")

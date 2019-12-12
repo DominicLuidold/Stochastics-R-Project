d4fcbb19d6cc6634faf5b0e3d439562379fab0c0
irina-grabhermeier <igr7222@students.fhv.at>
2019-12-12 13:25:40 +0100

    Update histogramme.R
    überall breaks=c(1,2,3,4,5) hinzugefügt

--- 400_R-Skripte/histogramme.R
+++ 400_R-Skripte/histogramme.R
@@ -1,29 +1,30 @@
-hist(data[,"Gesamteindruck"], xlab ="Schulnoten", main = "Histogram of Gesamteindruck")
+hist(data[,"Gesamteindruck"],breaks = c(1,2,3,4,5), xlab ="Schulnoten", main = "Histogram of Gesamteindruck")
 
-hist(data[,"Raum"], xlab ="Schulnoten",  main = "Histogram of Schulnoten ")
+hist(data[,"Raum"],breaks = c(1,2,3,4,5), xlab ="Schulnoten",  main = "Histogram of Schulnoten ")
 
-hist(data[,"Termin"], xlab ="Schulnoten",  main = "Histogram of Termin ")
+hist(data[,"Termin"],breaks = c(1,2,3,4,5), xlab ="Schulnoten",  main = "Histogram of Termin ")
 
-hist(data[,"Unterricht.abwechslungsr"], xlab ="Unterricht.abwechslungsr",  main = "Histogram of Unterricht.abwechslungsr")
+hist(data[,"Unterricht.abwechslungsr"],breaks = c(1,2,3,4,5), xlab ="Unterricht.abwechslungsr",  main = "Histogram of Unterricht.abwechslungsr")
 
-hist(data[,"Unterricht.motivierend"], xlab ="Unterricht.motivierend",  main = "Histogram of Unterricht.motivierend ")
+hist(data[,"Unterricht.motivierend"],breaks = c(1,2,3,4,5), xlab ="Unterricht.motivierend",  main = "Histogram of Unterricht.motivierend ")
 
-hist(data[,"Unterricht.Fragen"], xlab ="Unterricht.Fragen",  main = "Histogram of Unterricht.Fragen")
+hist(data[,"Unterricht.Fragen"],breaks = c(1,2,3,4,5), xlab ="Unterricht.Fragen",  main = "Histogram of Unterricht.Fragen")
 
-hist(data[,"Unterricht.strukturiert"], xlab ="Unterricht.strukturiert",  main = "Histogram of Unterricht.strukturiert ")
+hist(data[,"Unterricht.strukturiert"],breaks = c(1,2,3,4,5), xlab ="Unterricht.strukturiert",  main = "Histogram of Unterricht.strukturiert ")
 
-hist(data[,"Vorlesung.verstaendlich"], xlab ="Vorlesung.verstaendlich",  main = "Histogram of Vorlesung.verstaendlich ")
+hist(data[,"Vorlesung.verstaendlich"],breaks = c(1,2,3,4,5), xlab ="Vorlesung.verstaendlich",  main = "Histogram of Vorlesung.verstaendlich ")
 
-hist(data[,"Vorlesung.Geschwindigkeit"], xlab ="Vorlesung.Geschwindigkeit",  main = "Histogram of Vorlesung.Geschwindigkeit")
+hist(data[,"Vorlesung.Geschwindigkeit"],breaks = c(1,2,3,4,5), xlab ="Vorlesung.Geschwindigkeit",  main = "Histogram of Vorlesung.Geschwindigkeit")
 
-hist(data[,"Vorlesung.Praxis"], xlab ="Vorlesung.Praxis",  main = "Histogram of Vorlesung.Praxis ")
+hist(data[,"Vorlesung.Praxis"],breaks = c(1,2,3,4,5), xlab ="Vorlesung.Praxis",  main = "Histogram of Vorlesung.Praxis ")
 
-hist(data[,"Uebung.Beispiele"], xlab ="Uebung.Beispiele",  main = "Histogram of Uebung.Beispiele ")
+hist(data[,"Uebung.Beispiele"],breaks = c(1,2,3,4,5), xlab ="Uebung.Beispiele",  main = "Histogram of Uebung.Beispiele ")
 
-hist(data[,"Uebung.schwierig"], xlab ="Uebung.schwierig",  main = "Histogram of Uebung.schwierig ")
+hist(data[,"Uebung.schwierig"],breaks = c(1,2,3,4,5), xlab ="Uebung.schwierig",  main = "Histogram of Uebung.schwierig ")
 
 
 x <- c(data[,"Uebung.schwierig"], 4)
+
 
 barplot(x)
 

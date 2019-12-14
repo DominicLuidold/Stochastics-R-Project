f4c1800d61f06372af7a3940bcdf301f713de02c
Alexander Weber <mail@alexw.at>
2019-12-14 14:59:51 +0100

    Update correlation Analysis

--- 400_R-Skripte/correlationAnalysis.R
+++ 400_R-Skripte/correlationAnalysis.R
@@ -1,48 +1,37 @@
-
 #Zusammenhang Verständlichkeit und Geschwindigkeit der Vorlesung
-#Lineare Regressionsgerade bestimmen und 
-
 vorlesung_verstaendlich <- data[, "Vorlesung.verstaendlich"]
 vorlesung_geschwindigkeit <- data[, "Vorlesung.Geschwindigkeit"]
 
 
-plot(vorlesung_verstaendlich, vorlesung_geschwindigkeit, main = "ZH Vorlesung.Verstaendlich und Vorlesung.Geschwindigkeit")
-cor(vorlesung_verstaendlich, vorlesung_geschwindigkeit)
+corValue <- cor(vorlesung_verstaendlich, vorlesung_geschwindigkeit, use = "complete.obs")
+mainText <- paste("ZH Vorlesung.Verstaendlich und Vorlesung.Geschwindigkeit, cor:", round(corValue, 4))
 
-lm(vorlesung_geschwindigkeit ~ vorlesung_verstaendlich) #zeigt y=kx+d, Rheienfolge Ausgabe - Werte: d dann k
+plot(vorlesung_verstaendlich, vorlesung_geschwindigkeit, main = mainText)
 abline(lm(vorlesung_geschwindigkeit ~ vorlesung_verstaendlich)) #zeichnet lineare Regressionsgerade
 
-#Berechnung des Korrelationsfaktors r
 
-#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
-shapiro.test(vorlesung_verstaendlich)
-shapiro.test(vorlesung_geschwindigkeit)
 
-cor.test(vorlesung_verstaendlich, vorlesung_geschwindigkeit, method = "pearson")
+#Zusammenhang Vorlesung Verstaendlich und Vorlesung Praxis im Unterricht
+vorlesung_verstaendlich <- data[, "Vorlesung.verstaendlich"]
+vorlesung_praxis <- data[, "Vorlesung.Praxis"]
 
-# r = 0.8119...
+corValue <- cor(vorlesung_verstaendlich, vorlesung_praxis, use = "complete.obs")
+mainText <- paste("ZH Vorlesung.verstaendlich und Vorlesung.Praxis, cor:", round(corValue, 4))
+
+plot(vorlesung_verstaendlich, vorlesung_praxis, main = mainText)
+abline(lm(vorlesung_praxis ~ vorlesung_verstaendlich))
+
 
 
 #Zusammenhang Umfang der Beispiele und Schwierigkeit
-#lineare Regressionsgerade 
 umfang_beispiele <- data[, "Uebung.Beispiele"]
 schwierigkeit_beispiele <- data[, "Uebung.schwierig"]
 
-plot(umfang_beispiele, schwierigkeit_beispiele, main = "ZH Uebung.Beispiele und Uebung.schwierig")
-cor(umfang_beispiele, schwierigkeit_beispiele)
+corValue <- cor(umfang_beispiele, schwierigkeit_beispiele, use = "complete.obs")
+mainText <- paste("ZH Uebung.Beispiele und Uebung.schwierig, cor:", round(corValue, 4))
 
-lm(schwierigkeit_beispiele ~ umfang_beispiele)
+plot(umfang_beispiele, schwierigkeit_beispiele, main = mainText)
 abline(lm(schwierigkeit_beispiele ~ umfang_beispiele))
-
-#Korrelation
-shapiro.test(umfang_beispiele)
-shapiro.test(schwierigkeit_beispiele)
-
-#Bei Variable umfang_beispiele ist shapiro-Test unter 0.05, also verwenden wir spearman-Methode
-
-cor.test(umfang_beispiele, schwierigkeit_beispiele, method = "spearman")
-
-# r = rho = 0.6139...    --Warnung kann ignoriert werden
 
 
 
@@ -50,105 +39,44 @@ cor.test(umfang_beispiele, schwierigkeit_beispiele, method = "spearman")
 abwechlungsreich <- data[, "Unterricht.abwechslungsr"]
 motivierend <- data[, "Unterricht.motivierend"]
 
-plot(abwechlungsreich, motivierend, main = "Unterrich.abwechslungsreich und Unterricht.motivierend")
-cor(abwechlungsreich, motivierend)
+corValue <- cor(abwechlungsreich, motivierend, use = "complete.obs")
+mainText <- paste("ZH Unterrich.abwechslungsreich und Unterricht.motivierend, cor:", round(corValue, 4))
 
-lm(motivierend ~ abwechlungsreich) #zeigt y=kx+d, Rheienfolge Ausgabe - Werte: d dann k
+plot(abwechlungsreich, motivierend, main = mainText)
 abline(lm(motivierend ~ abwechlungsreich)) #zeichnet lineare Regressionsgerade
 
-#Berechnung des Korrelationsfaktors r
-
-#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
-shapiro.test(abwechlungsreich)
-shapiro.test(motivierend)
-
-cor.test(abwechlungsreich, motivierend, method = "pearson")
-
-# r = 0.43488...
 
 
 #Zusammenhang genügend Platz für Fragen und motivierend
-
 motivierend <- data[, "Unterricht.motivierend"]
 fragen <- data[, "Unterricht.Fragen"]
 
-plot(motivierend, fragen, main = "ZH Unterricht.motivierend und Unterricht.Fragen")
-cor(motivierend, fragen)
+corValue <- cor(motivierend, fragen, use = "complete.obs")
+mainText <- paste("ZH Unterricht.motivierend und Unterricht.Fragen, cor:", round(corValue, 4))
 
-lm(fragen ~ motivierend)
+plot(motivierend, fragen, main = mainText)
 abline(lm(fragen ~ motivierend))
 
 
-#Berechnung des Korrelationsfaktors r
-
-#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
-shapiro.test(motivierend)
-shapiro.test(fragen)
-
-cor.test(motivierend, fragen, method = "pearson")
-
-# r = 0.517...
 
 #Zusammenhang abwechslungsreich und strukturiert
-
 abwechlungsreich <- data[, "Unterricht.abwechslungsr"]
 strukturiert <- data[, "Unterricht.strukturiert"]
 
-plot(abwechlungsreich, strukturiert, main = "ZH Unterricht.abwechslungsreich und Unterricht.strukturiert")
-cor(abwechlungsreich, strukturiert)
+corValue <- cor(abwechlungsreich, strukturiert, use = "complete.obs")
+mainText <- paste("ZH Unterricht.abwechslungsreich und Unterricht.strukturiert, cor:", round(corValue, 4))
 
-lm(strukturiert ~ abwechlungsreich)
+plot(abwechlungsreich, strukturiert, main = mainText)
 abline(lm(strukturiert ~ abwechlungsreich))
 
-#Berechnung des Korrelationsfaktors r
-
-#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
-shapiro.test(abwechlungsreich)
-shapiro.test(strukturiert)
-
-cor.test(abwechlungsreich, strukturiert, method = "pearson")
-
-# r = 0.3625...
-
-#Zusammenhang Termin & Zeit und Geschwindigkeit im Unterricht
-
-termin <- data[, "Termin"]
-geschwindigkeit <- data[, "Vorlesung.Geschwindigkeit"]
-
-plot(termin, geschwindigkeit, main = "ZH Termin und Vorlesung.Geschwindigkeit")
-cor(termin, geschwindigkeit)
-
-lm(geschwindigkeit ~ termin)
-abline(lm(geschwindigkeit ~ termin))
-
-#Berechnung des Korrelationsfaktors r
-
-#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
-shapiro.test(termin)
-shapiro.test(geschwindigkeit)
-
-cor.test(termin, geschwindigkeit, method = "spearman")
-
-# r = rho = 0.0695...
 
 
 #Zusammenhang Gesamteindruck und Schwierigkeit im Unterricht
-
 gesamteindruck <- data[, "Gesamteindruck"]
 schwierigkeit <- data[, "Uebung.schwierig"]
 
-plot(gesamteindruck, schwierigkeit, main = "ZH Gesamteindruck und Uebung.schwierig")
-cor(gesamteindruck, schwierigkeit)
+corValue <- cor(gesamteindruck, schwierigkeit, use = "complete.obs")
+mainText <- paste("ZH Gesamteindruck und Uebung.schwierig, cor:", round(corValue, 4))
 
-lm(schwierigkeit ~ gesamteindruck)
+plot(gesamteindruck, schwierigkeit, main = mainText)
 abline(lm(schwierigkeit ~ gesamteindruck))
-
-#Berechnung des Korrelationsfaktors r
-
-#Wenn Werte > 0.05 nach Shapiro-Test, kann diese "Pearson-Methode" verwendet werden, weil normalverteilt
-shapiro.test(gesamteindruck)
-shapiro.test(schwierigkeit)
-
-cor.test(gesamteindruck, schwierigkeit, method = "pearson")
-
-# r = 0.6834...
